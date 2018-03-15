9b38a6e8e58208a1fd22d66fd967c660fbc11929
René Bärnreuther <rene.baernreuther@web.de>
2018-03-15 13:40:35 +0100

    Created empty javascript file with interface

---
+++ js/elimination.js
@@ -0,0 +1,7 @@
+/**
+ * Call this method when user presses the score button
+ * @param scoreForOnedart score shot by ONE dart
+ */
+function score(scoreForOnedart){
+    console.log("Score: " + scoreForOnedart)
+}
